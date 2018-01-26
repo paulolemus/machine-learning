716377a23bae09081967b8dd0559eeee3db2fdea
Paulo Lemus <paulolemus14@gmail.com>
2018-01-25 15:29:09 -1000

    Example for adding gen file to package main

--- plotter/__main__.py
+++ plotter/__main__.py
@@ -1,7 +1,11 @@
-
+from plotter import gen_margin_vs_training_error
 
 def main():
     print("Entered main")
+
+    # Plot generation functions
+    gen_margin_vs_training_error.main()
+
 
 if __name__ == '__main__':
     main()
---
+++ plotter/gen_margin_vs_training_error.py
@@ -0,0 +1,10 @@
+"""
+Generate margin vs training error.
+"""
+
+def main():
+    print("entered margin_vs_training_error")
+
+
+if __name__ == '__main__':
+    main()
