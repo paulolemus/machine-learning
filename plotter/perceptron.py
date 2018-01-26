fbeb6cf7189d45d31fae0f516fac6c6d6e31ad41
Paulo Lemus <paulolemus14@gmail.com>
2018-01-25 14:55:32 -1000

    Finished preliminary perceptron

--- plotter/perceptron.py
+++ plotter/perceptron.py
@@ -4,28 +4,42 @@ Contains the Perceptron class which can be trained and then used to linearly cla
 
 class Perceptron:
 
-    def __init__(self, dimensions: int):
+    def __init__(self, dimensions: int, learning_rate):
         # Using w[0] as bias weight
         self.bias = 1
         self.threshold = 0
         self.dimensions = dimensions
+        self.learning_rate = learning_rate
         self.weights = [0 for __ in range(dimensions+1)]
 
-    def sign(self, *inputs):
+    def sign(self, inputs):
+        """
+        Classify a single coordinate.
+        """
+
         # Guard mismatched vector size
         if len(inputs) != self.dimensions:
             raise ValueError
 
-        inputs.insert(self.bias, 0)
+        inputs.insert(0, self.bias)
         total = sum([x*w for x, w in zip(inputs, self.weights)])
 
         return 1 if total > self.threshold else -1
 
 
-    def train(self, coordinates):
+    def train(self, dataset):
         """
-        Provided a list of coordinates, where a coordinate consists of n input values
-        and a correct value, update the perceptron weights using each coordinate.
+        Provided a dataset, where a dataset consists of a list of coordinates paired
+        with their proper classification , update the perceptron weights using each
+        coordinate.
+
+        Acceptable format:
+        dataset: List[coordinates, label]
         """
+        for coordinates, label in dataset:
+            classification = self.sign(coordinates)
+            adjustment = self.learning_rate * (label - classification)
+
+            self.weights = [weight+adjustment for weight in self.weights]
 
 
